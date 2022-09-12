4e2342154219ef8687ff23ef4f548d1a52261043
jiashilong <jiashilong_syit@163.com>
2022-09-12 16:21:00 +0800

    Springboot优先处理Controller资源映射，否则转发给静态资源处理器

--- ming-spring-boot-intergration/src/main/java/com/ming/boot/intergration/controller/StaticController.java
+++ ming-spring-boot-intergration/src/main/java/com/ming/boot/intergration/controller/StaticController.java
@@ -1,2 +1,15 @@
-package com.ming.boot.intergration.controller;public class StaticController {
+package com.ming.boot.intergration.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/static")
+public class StaticController {
+
+    // @GetMapping("/p2.png")
+    public String p2() {
+        return "this is p2 page...";
+    }
 }
