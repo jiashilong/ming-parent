ac25967c883b89330ac97736c4aab85c04d60b49
jiashilong <jiashilong_syit@163.com>
2022-09-12 16:06:54 +0800

    添加静态资源映射关系。Springboot默认优选查找静态资源，找不到静态资源，再转发给Controller处理。

--- ming-spring-boot-intergration/src/main/java/com/ming/boot/intergration/configuration/WebMvcConfiguration.java
+++ ming-spring-boot-intergration/src/main/java/com/ming/boot/intergration/configuration/WebMvcConfiguration.java
@@ -1,15 +1,17 @@
 package com.ming.boot.intergration.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/intergration/static/**")
+        registry.addResourceHandler( "/static/**")
                 .addResourceLocations("classpath:/static/");
     }
 
